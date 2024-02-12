859c3e3296443ffb2ffb500f082d1a531ab62061
devbella <bellachoi1210@gmail.com>
2024-02-12 13:04:41 +0900

    apply chirpy

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1707676733',resources: [
+    cacheName: 'chirpy-1707677976',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
